d6761b6d0a413f64359dd33312e7d5e93caa2318
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-25 11:40:09 +0600

    added nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header/Header"
+
 function App() {
 
   return (
     <>
       <h1>React Donation Campaign</h1>
+      <Header />
+      <Outlet />
     </>
   )
 }
