0c27b358f06b7d5a4635e3a16d6a9e0ff62fd2a6
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-25 11:35:25 +0600

    maked essential components and pages and set up router into whole application

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import Donation from './pages/Donation/Donation.jsx'
+import Statistics from './pages/Statistics/Statistics.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/donation",
+        element: <Donation />
+      },
+      {
+        path: '/statistics',
+        element: <Statistics />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
