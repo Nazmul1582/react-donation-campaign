be56ad96d478815fdbd5fa71a5111f99a70de3fe
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-26 00:02:43 +0600

    installed recharts and created a demo pie chart for statistics page

--- src/pages/DonationDetails/DonationDetails.jsx
+++ src/pages/DonationDetails/DonationDetails.jsx
@@ -4,7 +4,7 @@ const DonationDetails = () => {
   return (
     <section>
       <div className="container mx-auto pt-20 pb-24">
-        <div className="">
+        <div>
           <div className="relative">
             <img className="rounded-lg" src={image} alt="" />
             <div className="bg-black h-32 absolute bottom-0 right-0 left-0 opacity-70 rounded-b-lg"></div>
@@ -13,17 +13,18 @@ const DonationDetails = () => {
             </div>
           </div>
           <div className="py-14">
-          <h2 className="text-4xl font-bold mb-6">Good Education</h2>
-          <p>
-            There are many things that can be done to ensure that all people
-            have access to a good education. Governments can invest in public
-            schools, provide financial assistance to students, and make sure
-            that all schools have qualified teachers and resources. Families can
-            support their children's education by creating a learning
-            environment at home and helping them with their schoolwork. Teachers
-            can create a positive and supportive learning environment for their
-            students and challenge them to reach their full potential.
-          </p>
+            <h2 className="text-4xl font-bold mb-6">Good Education</h2>
+            <p>
+              There are many things that can be done to ensure that all people
+              have access to a good education. Governments can invest in public
+              schools, provide financial assistance to students, and make sure
+              that all schools have qualified teachers and resources. Families
+              can support their children's education by creating a learning
+              environment at home and helping them with their schoolwork.
+              Teachers can create a positive and supportive learning environment
+              for their students and challenge them to reach their full
+              potential.
+            </p>
           </div>
         </div>
       </div>
--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,10 +1,66 @@
+import { Cell, Pie, PieChart } from "recharts";
 
 const Statistics = () => {
+  const data = [
+    { name: "Group A", value: 67 },
+    { name: "Group B", value: 33 },
+  ];
+
+  const COLORS = ["#FF444A", "#00C49F"];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
-        <div>
-            Statistics
-        </div>
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
     );
+  };
+
+  return (
+    <section>
+      <div className="container mx-auto pt-20 pb-24">
+        <div className="flex items-center justify-center">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Statistics;
