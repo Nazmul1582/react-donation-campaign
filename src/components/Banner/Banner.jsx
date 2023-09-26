92c07bbae0502aa3bfdac5574c84ee9ad47b97ae
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-26 13:58:13 +0600

    home page has maked responsive for all devices

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,9 +3,9 @@
 const Banner = () => {
   return (
     <section className="bg-banner-img bg-cover bg-no-repeat bg-center">
-      <div className="pt-36 pb-52 w-full h-full flex  justify-center items-center bg-white opacity-95">
+      <div className="pt-20 md:pt-36 pb-36 md:pb-52 w-full h-full flex  justify-center items-center bg-white opacity-95">
       <div className="container mx-auto">
-        <h2 className="text-center font-bold text-5xl mb-10">I Grow By Helping People In Need</h2>
+        <h2 className="text-center font-bold text-3xl md:text-5xl mb-10 leading-10">I Grow By Helping People In Need</h2>
         <div className="join flex justify-center">
           <input
             className="input input-bordered join-item focus:outline-0"
